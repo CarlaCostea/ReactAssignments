bb02693051747b6607c344598b7e8d7a8b28d2f0
Carla <carlacostea@yahoo.com>
2020-08-19 22:06:21 +0300

    add state

--- base-syntax--assignment-problem/src/App.js
+++ base-syntax--assignment-problem/src/App.js
@@ -4,22 +4,36 @@ import UserOutput from './UserOutput/UserOutput';
 import UserInput from './UserInput/UserInput';
 
 class App extends Component {
+  state = {
+    users: [
+      {userName: 'Carla'},
+      {userName: 'Cristina'},
+      {userName: 'Max'}
+    ]
+  };
+
+  switchUserName = (nickname) => {
+    this.setState({
+      users: [
+        {userName: 'Carluta'},
+        {userName: 'Grisinuta'},
+        {userName: 'Max'}
+      ]
+    })
+  }
   render() {
     return (
       <div className="App">
         <ol>
-          
-          <li>Add state to the App component (=> the username) and pass the username to the UserOutput component</li>
-          <li>Add a method to manipulate the state (=> an event-handler method)</li>
           <li>Pass the event-handler method reference to the UserInput component and bind it to the input-change event</li>
           <li>Ensure that the new input entered by the user overwrites the old username passed to UserOutput</li>
           <li>Add two-way-binding to your input (in UserInput) to also display the starting username</li>
           <li>Add styling of your choice to your components/ elements in the components - both with inline styles and stylesheets</li>
         </ol>
         <UserInput/>
-        <UserOutput userName="Carla"/>
-        <UserOutput/>
-        <UserOutput/>
+        <UserOutput userName = {this.state.users[0].userName}/>
+        <UserOutput userName = {this.state.users[1].userName}/>
+        <UserOutput userName = {this.state.users[2].userName}/>
       </div>
     );
   }
