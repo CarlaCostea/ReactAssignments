a3a48b7e7926f7ee3421a9552e4b8a0c21f41dca
Carla <carlacostea@yahoo.com>
2020-08-21 10:24:40 +0300

    validation component

---
+++ lists-conditionals--assignment-problem/src/ValidationComponent/ValidationComponent.js
@@ -0,0 +1,17 @@
+// Create an input field (in App component) with a change listener which outputs the length of the entered text below it (e.g. in a paragraph)
+// Create a new component (=> ValidationComponent) which receives the text length as a prop
+// Inside the ValidationComponent, either output "Text too short" or "Text long enough" depending on the text length (e.g. take 5 as a minimum length)
+
+import React from 'react'
+
+const validationCompoment = (props) => {
+    return (
+        <div>
+            {
+                (props.inputLength > 8) ? <p>Text long enough</p> : <p>Text too short</p>
+            }
+        </div>
+    )
+}
+
+export default validationCompoment;
